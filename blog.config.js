0f36306f3fdaec9456da068e2fbce209ffc32cbc
isngr <119165744+isngr@users.noreply.github.com>
2025-08-15 22:51:45 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'isn青奇', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一个只想躺平的人🛌', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://bk.219413.xyz', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://bk.219413.xyz', // 网站地址 例如 www.tangly1024.com
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'isn,青奇,isn青奇', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
