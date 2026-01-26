901df67d59134a5f231fd4d1ded378a5dfdcf6d1
isngr <119165744+isngr@users.noreply.github.com>
2026-01-26 18:10:29 +0800

    Update blog configuration with new author details

--- blog.config.js
+++ blog.config.js
@@ -15,20 +15,15 @@ const BLOG = {
   APPEARANCE: process.env.NEXT_PUBLIC_APPEARANCE || 'light', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'isn', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '🛌一个躺平v的干饭人🍚', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://isn.us.ci', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'isn, 博客', // 网站关键词 英文逗号隔开
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'NotionNext', // 您的昵称 例如 tangly1024
+  BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的干饭人🍚', // 作者简介
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
   BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
   BEI_AN_GONGAN: process.env.NEXT_PUBLIC_BEI_AN_GONGAN || '', // 公安备案号，例如 '浙公网安备3xxxxxxxx8号'
 
-// 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
-CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'isngr@live.com', // 邮箱地址 例如mail@tangly1024.com
-CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || 'https://github.com/isngr', // 你的github个人主页 例如 https://github.com/tangly1024
-CONTACT_QQ: process.env.NEXT_PUBLIC_CONTACT_QQ || '136530240', // 你的QQ号 例如 10241234
-    
   // RSS订阅
   ENABLE_RSS: process.env.NEXT_PUBLIC_ENABLE_RSS || true, // 是否开启RSS订阅功能
 
@@ -69,8 +64,8 @@ CONTACT_QQ: process.env.NEXT_PUBLIC_CONTACT_QQ || '136530240', // 你的QQ号 �
 
   // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语。
   GREETING_WORDS:
-    process。env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Hi，我是一个人, Hi，我就是一个人,Hi，我只是一个人,有来看我的人吗。🎉',
+    process.env.NEXT_PUBLIC_GREETING_WORDS ||
+    'Hi，我是一个程序员, Hi，我是一个打工人,Hi，我是一个干饭人,欢迎来到我的博客🎉',
 
   // uuid重定向至 slug
   UUID_REDIRECT: process.env.UUID_REDIRECT || false
